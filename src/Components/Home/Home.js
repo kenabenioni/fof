1a40c1768b7b247a66a63501bdfead8b30cb6963
kenabenioni <kenabenioni@gmail.com>
2018-12-04 08:34:15 -1000

    changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 import "./Home.css";
 import banner from "../../fof-img/DSC_0180-4.jpg";
+import play from "../../fof-img/DSC_0094.jpg";
+import book from "../../fof-img/DSC_0242.jpg";
+import step from "../../fof-img/DSC_0291.jpg";
+import past from "../../fof-img/DSC_0107.jpg";
+import waimeaElem from "../../fof-img/DSC_0240.jpg";
+import waimeaMid from "../../fof-img/DSC_0046 (2).jpg";
+import paauilo from "../../fof-img/DSC_0006-2.jpg";
 class Home extends Component {
   constructor(props) {
     super(props);
@@ -38,19 +45,52 @@ class Home extends Component {
           <img src={banner} className="banner-img" />
         </div>
         <div className="early">
-          <h2>EARLY CHILDHOOD</h2>
+          <h2 className="early-title">EARLY CHILDHOOD</h2>
           <div className="early-links">
             <div className="section">
+            <img src={play} className="section-img"/>
               <h4>Play and Learn Group</h4>
+              <p>Fill in text here.</p>
             </div>
             <div className="section">
+            <img src={book} className="section-img"/>
               <h4>Book Bag Project</h4>
+              <p>Fill in text here.</p>
             </div>
             <div className="section">
+            <img src={step} className="section-img"/>
               <h4>Step Up To Kindergarden</h4>
+              <p>Fill in text here.</p>
             </div>
             <div className="section">
+            <img src={past} className="section-img"/>
               <h4>Past Projects</h4>
+              <p>Fill in text here.</p>
+            </div>
+          </div>
+        </div>
+        <div className="programs">
+        <h2 className="programs-title">AFTER SCHOOL & SUMMER PROGRAMS</h2>
+        <div className="early-links">
+            <div className="section">
+            <img src={waimeaElem} className="section-img"/>
+              <h4>Waimea Elem. School</h4>
+              <p>Fill in text here.</p>
+            </div>
+            <div className="section">
+            <img src={waimeaMid} className="section-img"/>
+              <h4>Waimea Middle School</h4>
+              <p>Fill in text here.</p>
+            </div>
+            <div className="section">
+            <img src={paauilo} className="section-img"/>
+              <h4>Paauilo Elem. and Inter.</h4>
+              <p>Fill in text here.</p>
+            </div>
+            <div className="section">
+            <img src={past} className="section-img"/>
+              <h4>Honokaa High and Inter.</h4>
+              <p>Fill in text here.</p>
             </div>
           </div>
         </div>
