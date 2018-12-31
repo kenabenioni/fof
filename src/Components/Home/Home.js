ed0e32fb5b12adf725ab74c7e287569c04c87464
kenabenioni <kenabenioni@gmail.com>
2018-12-30 17:13:54 -1000

    changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import newban from '../../fof-img/IMG_1154.jpg';
+import playgroup from '../../fof-img/playgroup.jpg';
+import bookbag from '../../fof-img/IMG_4893.jpg';
+import kindergarden from '../../fof-img/IMG_5074.jpg';
+import pastproj from '../../fof-img/IMG_0832.JPG';
+import waimeaElem from '../../fof-img/IMG_2824.JPG';
 
 class Home extends Component {
   constructor(props) {
@@ -27,7 +33,7 @@ class Home extends Component {
           <Link to={"/playlearngroup"}>
             <div className="section">
               <img
-                src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/47432783_1912776392121337_2367629017164546048_o.jpg?_nc_cat=102&_nc_ht=scontent-lax3-1.xx&oh=d02a7cd9eb47762cfba0b74b9baa0da5&oe=5C99F851"
+                src={playgroup}
                 className="section-img"
                 />
               <h4>Play and Learn Group</h4>
@@ -36,7 +42,7 @@ class Home extends Component {
           </Link>
             <div className="section">
               <img
-                src="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/47360238_1912776628787980_3525079143878230016_o.jpg?_nc_cat=106&_nc_ht=scontent-lax3-2.xx&oh=0e0c1e3fe12a27b67e474a0393351844&oe=5CA27601"
+                src={bookbag}
                 className="section-img"
               />
               <h4>Book Bag Project</h4>
@@ -44,7 +50,7 @@ class Home extends Component {
             </div>
             <div className="section">
               <img
-                src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/47377773_1912776658787977_528186616976506880_o.jpg?_nc_cat=104&_nc_ht=scontent-lax3-1.xx&oh=b9def937605153747d1e83d2089e1098&oe=5C9C9123"
+                src={kindergarden}
                 className="section-img"
               />
               <h4>Step Up To Kindergarden</h4>
@@ -52,8 +58,9 @@ class Home extends Component {
             </div>
             <div className="section">
               <img
-                src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/47394695_1912776468787996_1977421583236464640_o.jpg?_nc_cat=108&_nc_ht=scontent-lax3-1.xx&oh=fca044bdf4476484c8a8e05fa0605e0e&oe=5C688FC6"
+                src={pastproj}
                 className="section-img"
+                id="changeimg"
               />
               <h4>Past Projects</h4>
               <p>Fill in text here.</p>
@@ -65,7 +72,7 @@ class Home extends Component {
           <div className="early-links">
             <div className="section">
               <img
-                src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/47286845_1912776542121322_707258904651759616_o.jpg?_nc_cat=105&_nc_ht=scontent-lax3-1.xx&oh=57242b4d7665d527c35fe6c916945260&oe=5C65E714"
+                src={waimeaElem}
                 className="section-img"
               />
               <h4>Waimea Elem. School</h4>
@@ -89,8 +96,9 @@ class Home extends Component {
             </div>
             <div className="section">
               <img
-                src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/47322302_1912776692121307_8916212721922342912_o.jpg?_nc_cat=103&_nc_ht=scontent-lax3-1.xx&oh=82d9c4da7c1c436f79ef61217249af31&oe=5CA2D335"
+                src={newban}
                 className="section-img"
+                id="changeimg"
               />
               <h4>Honokaa High and Inter.</h4>
               <p>Fill in text here.</p>
